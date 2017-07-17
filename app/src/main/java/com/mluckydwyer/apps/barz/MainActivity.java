73218be538e60cbd30ccedd817bffedaabc97a5c
Jason Senthil <senthil.jason@gmail.com>
2017-07-17 18:12:00 -0500

    lol now need to add permissions

--- app/src/main/java/com/mluckydwyer/apps/barz/MainActivity.java
+++ app/src/main/java/com/mluckydwyer/apps/barz/MainActivity.java
@@ -5,9 +5,13 @@ import android.content.pm.PackageManager;
 import android.hardware.Camera;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CAMERA = 200;
 
     private Camera camera;
     private CameraPreview cameraPreview;
@@ -15,8 +19,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
-        
+
         // Create an instance of Camera
         camera = getCameraInstance();
 
@@ -25,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
         FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
         preview.addView(cameraPreview);
     }
-
+    
     private boolean checkCameraHardware(Context context){
         if(context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA))
             return true;
@@ -37,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
         try{
             c = Camera.open();
         }catch(Exception e){
-
+            e.getStackTrace();
         }
         return c;
     }
