273447eb051bd8379fa331ccd83e1bef74104d03
Matt Dwyer <mluckydwyer@gmail.com>
2017-08-21 21:44:34 -0500

    Added gif encoding

--- app/src/main/java/com/mluckydwyer/apps/barz/BackgroundProcess.java
+++ app/src/main/java/com/mluckydwyer/apps/barz/BackgroundProcess.java
@@ -6,8 +6,8 @@ import android.os.Environment;
 import android.util.Log;
 import android.view.SurfaceHolder;
 
-import org.jcodec.common.model.ColorSpace;
-import org.jcodec.common.model.Picture;
+import com.bumptech.glide.gifencoder.AnimatedGifEncoder;
+
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.JavaCameraView;
@@ -25,9 +25,7 @@ import org.opencv.video.BackgroundSubtractor;
 import org.opencv.video.Video;
 
 import java.io.File;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 /**
@@ -94,56 +92,19 @@ public class BackgroundProcess extends JavaCameraView implements SurfaceHolder.C
         int w = inputFrame.width();
         int h = inputFrame.height();
 
-        //resize image for fps purposes lol
-        //Imgproc.resize(inputFrame, inputFrame, new Size(256, 144));
-
-        //convert to HSV color space for swag purposes (test with this)
-        //Imgproc.cvtColor(inputFrame, inputFrame, Imgproc.COLOR_RGB2HSV, 3);
-
-        //makes image go to black and white
-        //Mat toGray = new Mat();
-        //Imgproc.cvtColor(inputFrame, inputFrame, Imgproc.COLOR_RGB2GRAY);
-
-        //do binary thres first
-        //Imgproc.threshold(inputFrame, inputFrame, 127, 255, Imgproc.THRESH_BINARY);
-
-        //gaussian blur
-        //Imgproc.GaussianBlur(inputFrame, inputFrame, new Size(5,5), 0);
-
-        //now threshold da image
-        //Imgproc.threshold(inputFrame, inputFrame, 138, 255, Imgproc.THRESH_BINARY);
-        //Imgproc.threshold(inputFrame, inputFrame, 0, 255, Imgproc.THRESH_OTSU);
-
-        //create an erosion and dilation matrixes
-        //Mat erosion = Imgproc.getStructuringElement(Imgproc.CV_SHAPE_RECT, new Size(3,3));
-        //Mat dilation = Imgproc.getStructuringElement(Imgproc.CV_SHAPE_RECT, new Size(8,8));
-
-        //now erode and dilate the toGray matrix
-        //Imgproc.erode(inputFrame, inputFrame, erosion);
-        //Imgproc.erode(toGray, toGray, erosion);
-        //Imgproc.dilate(inputFrame, inputFrame, dilation);
-        //Imgproc.dilate(toGray, toGray, dilation);
-
-        //Imgproc.resize(toGray, toGray, new Size(w, h));
-
-        //create region of interest and the bar from that for swag
         List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
         List<MatOfPoint> barPoints = new ArrayList<MatOfPoint>();
         List<MatOfInt> hulls = new ArrayList<MatOfInt>();
         Mat erosion = Imgproc.getStructuringElement(Imgproc.CV_SHAPE_RECT, new Size(5, 5));
         Mat dilation = Imgproc.getStructuringElement(Imgproc.CV_SHAPE_RECT, new Size(8, 8));
-        Rect roi = new Rect(w/3, 0, 20, h);
+        Rect roi = new Rect(w / 3, 0, 20, h);
         Mat bar = new Mat(inputFrame, roi);
         Mat maskedBar = new Mat();
         Mat barArea = bar.clone();
 
-        //barPoints.add(new MatOfPoint())
-
-
-
         Imgproc.cvtColor(bar, bar, Imgproc.COLOR_RGB2HSV, 3);
         Imgproc.cvtColor(bar, bar, Imgproc.COLOR_RGB2GRAY);
-        Imgproc.GaussianBlur(bar, bar, new Size(5,5), 0);
+        Imgproc.GaussianBlur(bar, bar, new Size(5, 5), 0);
         Imgproc.threshold(bar, bar, 0, 255, Imgproc.THRESH_OTSU);
 
         Imgproc.erode(bar, bar, erosion);
@@ -153,46 +114,16 @@ public class BackgroundProcess extends JavaCameraView implements SurfaceHolder.C
         Mat backsub = new Mat();
         backgroundSubtractor.apply(barArea, backsub);
 
-        //Imgproc.cvtColor(backsub, backsub, Imgproc.COLOR_RGB2HSV, 3);
-        //Imgproc.cvtColor(backsub, backsub, Imgproc.COLOR_RGB2GRAY);
-
         Mat m = new Mat();
         Core.subtract(bar, backsub, bar);
         Core.subtract(barArea, Core.mean(inputFrame), m);
         Imgproc.cvtColor(m, m, Imgproc.COLOR_RGB2HSV, 3);
         Imgproc.cvtColor(m, m, Imgproc.COLOR_RGB2GRAY);
-        //Core.subtract(bar, m, bar);
-        //Imgproc.findContours(bar, contours, new Mat(), Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
-        //Imgproc.fillConvexPoly(bar, barPoints, new Scalar(255.0, 255.0, 255.0));
-        //Imgproc.drawContours(bar, contours, 1, new Scalar(255.0, 255.0, 255.0));
-        //Log.d(TAG, "Contours: " + contours.size());
-
-        //Imgproc.cvtColor(bar, bar, Imgproc.);
-
-        //Core.bitwise_not(bar, bar);
 
         Mat submat = inputFrame.submat(roi);
         barArea.copyTo(maskedBar, bar);
 
         double buff[] = new double[maskedBar.channels()];
-
-        /*for (int x = 0; x < maskedBar.rows(); x++) {
-            for (int y = 0; y < maskedBar.cols(); y++) {
-                if (Arrays.equals(maskedBar.get(x, y), new double[]{0.0, 0.0, 0.0, 0.0})) {
-                    Mat sub = maskedBar.submat(x, x, y, y);
-                    sub = barArea.submat(x, x, y, y);
-                    double[] pixel = maskedBar.get(x, y);
-                    pixel[0] = 255.0;
-                    pixel[1] = 255.0;
-                    pixel[2] = 255.0;
-                    pixel[3] = 255.0;
-                    Log.d(TAG, "" + maskedBar.get(x, y, buff));
-                    //Log.d(TAG, maskedBar.get(x, y).length + "\t" + maskedBar.get(x, y)[0] + "\t" + maskedBar.get(x, y)[1] + "\t" + maskedBar.get(x, y)[2] + "\t" + maskedBar.get(x, y)[3]);
-                }
-            }
-        }*/
-
-        //Core.bitwise_not(maskedBar, maskedBar);
 
         maskedBar.copyTo(submat);
 
@@ -218,80 +149,42 @@ public class BackgroundProcess extends JavaCameraView implements SurfaceHolder.C
         return imgMasked;
     }
 
-    public String compileVideo() {
+    public void compileVideo() {
         Mat tmp = new Mat(ocvCameraView.getHeight(), ocvCameraView.getWidth(), CvType.CV_8U, new Scalar(4));
-        org.jcodec.api.awt.SequenceEncoder encoder = null;
-        File videoFile = new File(Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_PICTURES), "MyCameraApp");
-        String path = videoFile.getPath() + File.separator +
-                "VID_" + new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date()) + ".mp4";
+        ArrayList<Bitmap> videoBitmaps = new ArrayList<Bitmap>();
+        Bitmap bmp = null;
 
-        if (!videoFile.exists()) {
-            if (!videoFile.mkdirs()) {
-                Log.d("MyCameraApp", "failed to create directory");
-                return "Failed to create Directory";
-            }
+        Log.e(TAG, "VFs: " + videoFrames.size());
+
+        for (Mat frame : videoFrames) {
+            tmp = openCVFinal(frame);
+            bmp = Bitmap.createBitmap(tmp.cols(), tmp.rows(), Bitmap.Config.ARGB_8888);
+            Utils.matToBitmap(tmp, bmp);
+            videoBitmaps.add(bmp);
+            Log.e(TAG, "Frame Rendered" + bmp.getWidth() + "=" + tmp.width() + "\t" + bmp.getHeight() + "=" + tmp.height());
         }
+        videoFrames.clear();
 
+        Log.e(TAG, "Frames: " + videoBitmaps.size());
+
+        makeGIF(videoBitmaps);
+    }
+
+    public void makeGIF(ArrayList<Bitmap> videoBitmaps) {
         try {
-            encoder = new org.jcodec.api.awt.SequenceEncoder(new File(path));
-            ArrayList<Bitmap> videoBitmaps = new ArrayList<Bitmap>();
-            Bitmap bmp = null;
-
-            Log.e(TAG, "VFs: " + videoFrames.size());
-
-            for (Mat frame : videoFrames) {
-                tmp = openCVFinal(frame);
-                bmp = Bitmap.createBitmap(tmp.cols(), tmp.rows(), Bitmap.Config.ARGB_8888);
-                Utils.matToBitmap(tmp, bmp);
-                videoBitmaps.add(bmp);
-                Log.e(TAG, "Frame Rendered" + bmp.getWidth() + "=" + tmp.width() + "\t" + bmp.getHeight() + "=" + tmp.height());
-            }
-            videoFrames.clear();
-
-            Log.e(TAG, "Frames: " + videoBitmaps.size());
-
-            for (Bitmap bmpFrame : videoBitmaps) {
-                /*int[][] pixelData = new int[3][bmpFrame.getWidth() * bmpFrame.getHeight()];
-
-                Log.e(TAG, pixelData.length + " " + pixelData[0].length + "\t" + bmpFrame.getWidth() + " " + bmpFrame.getHeight());
-
-                for (int x = 0; x < bmpFrame.getWidth(); x++) {
-                    for (int y = 0; y < bmpFrame.getHeight(); y++) {
-                        pixelData[0][bmpFrame.getWidth() * y + x] = Color.red(bmpFrame.getPixel(x, y));
-                        pixelData[1][bmpFrame.getWidth() * y + x] = Color.green(bmpFrame.getPixel(x, y));
-                        pixelData[2][bmpFrame.getWidth() * y + x] = Color.blue(bmpFrame.getPixel(x, y));
-                    }
-                }*/
-
-                Picture dst = Picture.create(bmpFrame.getWidth(), bmpFrame.getHeight(), ColorSpace.RGB);
-                int[] dstData = dst.getPlaneData(0);
-                int off = 0;
-
-                for (int i = 0; i < bmpFrame.getHeight(); ++i) {
-                    for (int j = 0; j < bmpFrame.getWidth(); ++j) {
-                        int rgb1 = bmpFrame.getPixel(j, i);
-                        dstData[off++] = rgb1 >> 16 & 255;
-                        dstData[off++] = rgb1 >> 8 & 255;
-                        dstData[off++] = rgb1 & 255;
-                    }
-                }
-
-
-                //Picture pic = new Picture(bmpFrame.getWidth(), bmpFrame.getHeight(), pixelData, ColorSpace.RGB);
-
-                Log.e(TAG, "Frame encoded: " + dst.getWidth() + "=" + bmpFrame.getWidth() + "\t" + dst.getHeight() + "=" + bmpFrame.getHeight());
-                encoder.encodeNativeFrame(dst);
-            }
-
-
-            encoder.finish();
-
+            File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) + "/myimages/popout.gif");
+            file.delete();
         } catch (Exception e) {
-            e.printStackTrace();
         }
 
-        return videoFile.getPath();
+        AnimatedGifEncoder e = new AnimatedGifEncoder();
+        e.start(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) + "/myimages/popout.gif");
+        e.setDelay(250);
+
+        for (Bitmap b : videoBitmaps)
+            e.addFrame(b);
+
+        e.finish();
     }
 
 }
